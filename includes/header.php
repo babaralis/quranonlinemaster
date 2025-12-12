f5dc111dadb84339617c5863da8a60162ee91416
Taha <staha218@hotmail.com>
2025-12-12 19:51:25 +0500

    ok

--- includes/header.php
+++ includes/header.php
@@ -12,24 +12,32 @@
   <!-- Bootstrap CSS -->
   <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
+    rel="stylesheet" media="all" defer as="style" onload="this.onload=null;this.rel='stylesheet'"
   />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" media="all" defer as="style">
 
   <!-- Bootstrap Icons -->
   <link
     rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" media="all" defer as="style"
   />
-  <!-- Google Fonts -->
+    <!-- Google Fonts -->
   <link
+    rel="preload"
     href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Scheherazade+New:wght@400;700&display=swap"
-    rel="stylesheet"
+    as="style"
+    onload="this.onload=null;this.rel='stylesheet'"
   />
+  <noscript>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Scheherazade+New:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+  </noscript>
 
- <link rel="stylesheet" type="text/css" href="assets/css/style.css?v=1.2">
+ <link rel="stylesheet" type="text/css" href="assets/css/style.css?v=1.2" media="all" defer as="style">
 
  
 </head>
